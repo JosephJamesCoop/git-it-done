4b0d7572bd3c734f8cd51330da41efb158c0739d
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-12-11 16:10:16 -0800

    linked the second page to load issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,75 +10,76 @@ var getUserRepos = function (user) {
     //make request to url
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-          });
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
         } else {
             alert("Error: GitHub User Not Found")
         }
     })
-    .catch(function(error) {
-        alert("Unable to connect to GitHub");
-    });
+        .catch(function (error) {
+            alert("Unable to connect to GitHub");
+        });
 };
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
 
-if (username) {
-  getUserRepos(username);
-  nameInputEl.value = "";
-} else {
-  alert("Please enter a GitHub username");
-}
-  };
+    if (username) {
+        getUserRepos(username);
+        repoContainerEl.textContent = "";
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
 
-  var displayRepos = function(repos, searchTerm) {
-      if (repos.length === 0) {
-          repoContainerEl.textContent = "No repositories found. ";
-          return;
-      }
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found. ";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     // this will clear out all of the old content
-repoContainerEl.textContent = "";
-repoSearchTerm.textContent = searchTerm;
 
-// loop over repos
-for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
-  
-    // create a container for each repo
-    var repoEl = document.createElement("div");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-  
-    // create a span element to hold repository name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
-  
-    // append to container
-    repoEl.appendChild(titleEl);
-  
-    // create a status element
-var statusEl = document.createElement("span");
-statusEl.classList = "flex-row align-center";
+    repoSearchTerm.textContent = searchTerm;
 
-// check if current repo has issues or not
-if (repos[i].open_issues_count > 0) {
-  statusEl.innerHTML =
-    "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-} else {
-  statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-}
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-// append to container
-repoEl.appendChild(statusEl);
-    // append container to the dom
-    repoContainerEl.appendChild(repoEl);
-  }
-  };
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
 
 
 
-  userFormEl.addEventListener("submit", formSubmitHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);
